fe392ab1f2e66d55f331641314f0d26a808fb20e
Sz-Ting, Chen <kimikamis@gmail.com>
2019-02-03 22:20:41 +0800

    index list ordered by created time

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [ :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order('created_at DESC')
   end
 
   def new
