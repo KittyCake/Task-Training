1a6998fdcb940d2186e3f68f816ecd750ef50826
Sz-Ting, Chen <kimikamis@gmail.com>
2019-02-11 11:11:43 +0800

    can select status in fomr

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :user
+  STATUSES = %w(待處理 進行中 完成)
 
   validates :title, presence: true, length: { in: 1..20 }
   validates :content, presence: true, length: { in: 1..60 }
   validates :priority, inclusion: { in: %w(輕鬆 一般 重要) }
   validate :endtime_cannot_be_in_the_past
-  validates :status, presence: true, inclusion: { in: %w(待處理 進行中 完成) }
+  validates :status, presence: true, inclusion: { in: STATUSES }
 
   def endtime_cannot_be_in_the_past
     if endtime.present? && endtime < Time.now
